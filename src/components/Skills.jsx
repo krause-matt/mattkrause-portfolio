c86f692edf2a472c857a998df8dd4d94b48ab241
Matt Krause <krause.matt@pm.me>
2022-03-09 21:12:14 -0500

    Beginning on skills component and adding dev icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,33 @@
 import "./skills.scss";
 
 export default function Skills() {
-  return <div className="skills" id="skills"></div>;
+  const devicons = [
+    "devicon-bootstrap-plain",
+    "devicon-css3-plain",
+    "devicon-express-original",
+    "devicon-firebase-plain",
+    "devicon-git-plain",
+    "devicon-github-original",
+    "devicon-html5-plain",
+    "devicon-javascript-plain",
+    "devicon-mongodb-plain",
+    "devicon-nodejs-plain",
+    "devicon-python-plain",
+    "devicon-react-original",
+    "devicon-redux-original",
+    "devicon-sass-original",
+    "devicon-vscode-plain",
+  ];
+
+  return (
+    <div className="skills" id="skills">
+      {devicons.map((icon) => {
+        return (
+          <div className="icon-block">
+            <i className={icon}></i>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
