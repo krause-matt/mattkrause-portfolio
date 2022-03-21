bae19ce6df7123db19cae1b52e4901d15d442fd1
Matt Krause <krause.matt@pm.me>
2022-03-20 20:53:53 -0400

    Planning to re-design about section and have almost finished adding interactive details to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,7 +19,26 @@ export default function Skills() {
     { icon: "devicon-sass-original", skill: "Sass" },
   ];
 
-  const bootstrap = "version 4 and 5!";
+  const skillDesc = {
+    bootstrap: "I've used v4/v5 to create attractive and responsive sites.",
+    css: "My projects have been enhanced with media queries, flexbox/grid, and animations.",
+    express:
+      "I used express as a REST API server for my Ballpark Rater CRUD app: Check it out.",
+    firebase:
+      "I taught myself how to utilize when setting up an orders database for my React Pizza Store app: Check it out.",
+    git: "I utilize for version control, documentation, and establishing remotes on all of my apps.",
+    github:
+      "I've been contributing my code almost daily since learning the basics in Oct. 2021. Visit my GitHub.",
+    html: "I am currently working to improve my use of proper semantics and learn more attributes.",
+    javascript:
+      "I have completed two JavaScript courses totaling over 40 hours, and have registerd for an advanced topics course as well. JavaScript is used in all of my projects.",
+    mongodb:
+      "Mongo is the DB for my Ballpark Rater CRUD app which uses three schemas: Check it out.",
+    nodejs:
+      "I use node to run JavaScript and npm to add necessary libraries and frameworks to my projects.",
+    python:
+      "I use Python at work for data analysis/visualization projects. Not used explicitly for web development, but I feel it enhances my overall coding skill.",
+  };
 
   const [currentSkill, setCurrentSkill] = useState(null);
   const [skillDetails, setSkillDetails] = useState(null);
@@ -27,10 +46,40 @@ export default function Skills() {
   useEffect(() => {
     switch (skillDetails) {
       case "bootstrap":
-        setSkillDetails(bootstrap);
+        setSkillDetails(skillDesc.bootstrap);
+        break;
+      case "css":
+        setSkillDetails(skillDesc.css);
+        break;
+      case "express":
+        setSkillDetails(skillDesc.express);
+        break;
+      case "firebase":
+        setSkillDetails(skillDesc.firebase);
+        break;
+      case "git":
+        setSkillDetails(skillDesc.git);
+        break;
+      case "github":
+        setSkillDetails(skillDesc.github);
+        break;
+      case "html":
+        setSkillDetails(skillDesc.html);
+        break;
+      case "javascript":
+        setSkillDetails(skillDesc.javascript);
+        break;
+      case "mongodb":
+        setSkillDetails(skillDesc.mongodb);
+        break;
+      case "node.js":
+        setSkillDetails(skillDesc.nodejs);
+        break;
+      case "python":
+        setSkillDetails(skillDesc.python);
         break;
       default:
-        setSkillDetails("Click a skill to see more!");
+        setSkillDetails("Click a skill for more!");
     }
   }, [currentSkill]);
 
@@ -71,7 +120,7 @@ export default function Skills() {
             {skillDetails}
             <div
               className={
-                skillDetails === "Click a skill to see more!"
+                skillDetails === "Click a skill for more!"
                   ? "cursor active"
                   : "cursor"
               }
