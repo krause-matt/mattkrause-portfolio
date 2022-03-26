06f52b0394eedabe7b7aaa75809e808981a91a79
Matt Krause <krause.matt@pm.me>
2022-03-26 15:07:09 -0400

    wrote the About section content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,6 +44,28 @@ export default function About() {
         </div>
         <div className="right">
           <div className="aboutme">About Me.</div>
+          <p>
+            I'm an Electrical Engineer transitioning into Front-End Web
+            Development.
+            <br></br>
+            <br></br>
+            My introduction to web development came mid-2021 after researching
+            how to build a website for a work project. Along with learning the
+            latest web development languages and tools, I discovered that
+            creating useful, interactive apps simultaneously challenged my
+            logical and creative sides in an exciting way I hadn't experienced
+            before. Since then, I have been coding on daily basis to enhance my
+            web development skills and portfolio of personal projects.
+            <br></br>
+            <br></br>I have spent my professional career solving problems for
+            some of the world's largest brands including Samsung, Amazon, and
+            LG. My experience and determination make me an ideal candidate for a
+            team in need of a developer eager to learn and contribute.
+            <br></br>
+            <br></br>
+            Thanks for visiting, and feel free to{" "}
+            <a href="#contact">contact me</a> if you would like to learn more!
+          </p>
         </div>
       </div>
     </div>
