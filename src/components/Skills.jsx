e9bfef63b33e9f520ce7d53b0ba2643a99576843
Matt Krause <krause.matt@pm.me>
2022-04-02 15:42:15 -0400

    Added dynamic project links to the skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -24,74 +24,121 @@ export default function Skills() {
       "I've used Bootstrap v4 and v5 to create attractive and responsive sites.",
     css: "My projects have been enhanced with CSS media queries, flexbox/grid, and animations.",
     express:
-      "I used Express as a REST API server for my Ballpark Rater CRUD app: Check it out.",
+      "I used Express as a REST API server for my Ballpark Rater CRUD app. Check it out:",
     firebase:
-      "I taught myself how to utilize Google Firebase when setting up an orders database for my React Pizza Store app: Check it out.",
+      "I taught myself how to utilize Google Firebase when setting up an orders database for my React Pizza Store app. Check it out:",
     git: "I utilize Git for version control, documentation, and establishing remotes on all of my apps.",
     github:
-      "I've been contributing my code to GitHub almost daily since learning the basics in Oct. 2021. Visit my GitHub.",
+      "I've been contributing my code to GitHub almost daily since learning the basics in Oct. 2021.",
     html: "I am currently working to improve my use of proper semantics and learn more attributes.",
     javascript:
       "I have completed two JavaScript courses totaling over 40 hours, and have registerd for an advanced topics course as well. JavaScript is used in all of my projects.",
     mongodb:
-      "Mongo is the DB for my Ballpark Rater CRUD app which uses three schemas: Check it out.",
+      "Mongo is the DB for my Ballpark Rater CRUD app which uses three schemas. Check it out:",
     nodejs:
       "I use node to run JavaScript code and npm to add necessary libraries and frameworks to my projects.",
     python:
-      "I use Python at work for data analysis/visualization projects. Not used explicitly for web development, but I feel it enhances my overall coding skill.",
+      "I use Python at work for data analysis and visualization projects. Not used explicitly for web development, but I feel it enhances my overall coding skill.",
     react:
       "Three projects listed below (plus the site you're on right now!) were built to be interactive and seamless using React state management and Hooks.",
     redux:
-      "My Garden Plotter app (check it out) has a larger component and state system which allowed me to learn and deploy Redux.",
+      "My Garden Plotter app has a larger component and state system which allowed me to learn and deploy Redux. Check it out:",
     sass: "I enjoy taking advantage of Sass nesting and global variables to make styling easier.",
+  };
+
+  const skillLinks = {
+    link: {
+      express: "#skills",
+      firebase: "#skills",
+      github: "#skills",
+      mongodb: "#skills",
+      redux: "#skills",
+    },
+    text: {
+      express: "Ballpark Rater App",
+      firebase: "Pizza Place App",
+      github: "Visit my GitHub",
+      mongodb: "Ballpark Rater App",
+      redux: "Garden Plotter App",
+    },
   };
 
   const [currentSkill, setCurrentSkill] = useState(null);
   const [skillDetails, setSkillDetails] = useState(null);
+  const [skillLink, setSkillLink] = useState(null);
+  const [skillLinkText, setSkillLinkText] = useState(null);
 
   useEffect(() => {
     switch (skillDetails) {
       case "bootstrap":
         setSkillDetails(skillDesc.bootstrap);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "css":
         setSkillDetails(skillDesc.css);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "express":
         setSkillDetails(skillDesc.express);
+        setSkillLink(skillLinks.link.express);
+        setSkillLinkText(skillLinks.text.express);
         break;
       case "firebase":
         setSkillDetails(skillDesc.firebase);
+        setSkillLink(skillLinks.link.firebase);
+        setSkillLinkText(skillLinks.text.firebase);
         break;
       case "git":
         setSkillDetails(skillDesc.git);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "github":
         setSkillDetails(skillDesc.github);
+        setSkillLink(skillLinks.link.github);
+        setSkillLinkText(skillLinks.text.github);
         break;
       case "html":
         setSkillDetails(skillDesc.html);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "javascript":
         setSkillDetails(skillDesc.javascript);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "mongodb":
         setSkillDetails(skillDesc.mongodb);
+        setSkillLink(skillLinks.link.mongodb);
+        setSkillLinkText(skillLinks.text.mongodb);
         break;
       case "node.js":
         setSkillDetails(skillDesc.nodejs);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "python":
         setSkillDetails(skillDesc.python);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "react":
         setSkillDetails(skillDesc.react);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       case "redux":
         setSkillDetails(skillDesc.redux);
+        setSkillLink(skillLinks.link.redux);
+        setSkillLinkText(skillLinks.text.redux);
         break;
       case "sass":
         setSkillDetails(skillDesc.sass);
+        setSkillLink(null);
+        setSkillLinkText(null);
         break;
       default:
         setSkillDetails("Click a skill for more!");
@@ -128,8 +175,8 @@ export default function Skills() {
           );
         })}
       </div>
-
       <div className="details">
+        <div className="skills">Skills.</div>
         <div className="terminal">
           <p>
             {skillDetails}
@@ -143,6 +190,9 @@ export default function Skills() {
               |
             </div>
           </p>
+          <a href={skillLink} className="skill-link">
+            {skillLinkText}
+          </a>
         </div>
       </div>
     </div>
