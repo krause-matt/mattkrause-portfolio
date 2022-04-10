120c56ed3bf1f65aa4ba61e7f242780e77e0672c
Matt Krause <krause.matt@pm.me>
2022-04-09 22:00:06 -0400

    Beginning work on hamburger button NavBar dropdown for screen widths of 760px and below

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,23 @@
 import "./navbar.scss";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
+  const [dropDown, setDropDown] = useState(false);
+
   return (
     <div className="navbar">
       <div className="container">
+        <div
+          className="hamburger"
+          onClick={() => {
+            setDropDown(!dropDown);
+            console.log("dropDown", dropDown);
+          }}
+        >
+          <span className="line-1"></span>
+          <span className="line-2"></span>
+          <span className="line-3"></span>
+        </div>
         <ul className="links">
           <li>
             <a href="#about">about</a>
@@ -20,6 +34,17 @@ export default function NavBar() {
           <li>
             <a href="#contact">contact</a>
           </li>
+        </ul>
+      </div>
+      <div className={dropDown ? "dropdown active" : "dropdown inactive"}>
+        <ul className="dropdown-links">
+          <li className="dropdown-link-1">
+            <a href="#about">About</a>
+          </li>
+          <li className="dropdown-link-2">Skills</li>
+          <li className="dropdown-link-3">Projects</li>
+          <li className="dropdown-link-4">Resume</li>
+          <li className="dropdown-link-5">Contact</li>
         </ul>
       </div>
     </div>
